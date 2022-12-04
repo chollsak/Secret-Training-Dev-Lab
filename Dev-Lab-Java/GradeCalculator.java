5191235fe468f15a35e7cc4487e412805dd4a824
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-04 14:30:32 +0700

    add : Grade Calculator

--- Dev-Lab-Java/GradeCalculator.java
+++ Dev-Lab-Java/GradeCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ABProblem {
+public class GradeCalculator {
     public static void main(String[] args) {
         Scanner myInput = new Scanner(System.in);
 
