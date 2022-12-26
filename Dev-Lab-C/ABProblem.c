a34dbaa2e0ca5c86aae9ce303724fff3f61365c6
chollsakn <koonnew8688.cookie@gmail.com>
2022-12-27 01:02:35 +0700

    task10

---
+++ Dev-Lab-C/ABProblem.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+	int a,b ;
+  scanf("%d %d", &a,&b);
+	printf("%d", a+b);
+    return 0;
+}
